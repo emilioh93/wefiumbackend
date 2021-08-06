59a93f44e27424bfb18cb925278bfac7115bcfa0
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-08-06 20:29:45 -0300

    agregué meddlewares + index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
+/* --------------------------------- Config --------------------------------- */
+
+/* ---------------------------- Instancia express --------------------------- */
 const app = express();
 
-const port = 4000;
+// Crear puerto
+app.set("port", process.env.PORT || 4000);
 
-app.listen(port, ()=>{
-    console.log("Estoy usando puerto " + port);
-})
+app.listen(app.get("port"), () => {
+  console.log("Estoy usando puerto " + app.get("port"));
+  console.log(path.join(__dirname, "../public"));
+});
+
+/* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+/* ---------------------------------- Ruta --------------------------------- */
+app.get("/", (req, res) => {
+  res.send("Hola desde el servidor");
+});
