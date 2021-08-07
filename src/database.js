b89680f848bc8ebb28a9d5a23cc3eb4c67b10f8b
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-08-06 21:07:33 -0300

    agregué conexión a la base de datos

---
+++ src/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const url =
+  "mongodb+srv://emilio_wefium:pruebawefium@cluster0.kmfvh.mongodb.net/test";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("BD conectada");
+});
